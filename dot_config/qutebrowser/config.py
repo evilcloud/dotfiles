89684adf64b83f8250171a57871d72076eb4df93
blacksquare <evildatacloud@gmail.com>
2023-09-09 22:53:41 +0800

    yet another attempt at getting rid of private

---
+++ dot_config/qutebrowser/config.py
@@ -0,0 +1,18 @@
+# import catppuccin
+# 
+# config.load_autoconfig()
+# 
+# catppuccin.setup(c, "mocha", True)
+# 
+
+import dracula.draw
+
+dracula.draw.blood(c, {
+    "spacing":{
+        "vertical": 6,
+        "horizontal": 8
+        }
+    })
+
+config.load_autoconfig()
+
